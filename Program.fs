0fcf13741cdd9c8606c60c88ab46d70394bba1ee
Isaac Aronson <i@pingas.org>
2021-09-02 00:12:44 -0500

    Add async main pattern

--- Program.fs
+++ Program.fs
@@ -1,5 +1,14 @@
 open System
+open System.Threading.Tasks
+
+let mainTask = async { () }
 
 [<EntryPoint>]
-let main _ =
+let main argv =
+    async {
+        do! Async.SwitchToThreadPool()
+        return! mainTask
+    }
+    |> Async.RunSynchronously
+
     0 // successful exit code
