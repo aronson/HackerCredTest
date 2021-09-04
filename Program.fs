ca13680a32a27cb8f68aec85a994ca9a7400b92d
Isaac Aronson <i@pingas.org>
2021-09-03 19:34:22 -0500

    Filter messages by channel and log to console

--- Program.fs
+++ Program.fs
@@ -5,12 +5,18 @@ open Discord
 open System.Threading.Tasks
 open System.Threading
 
+let token =
+    Environment.GetEnvironmentVariable("TOKEN")
+
+let channelId =
+    uint64 (Environment.GetEnvironmentVariable("CHANNEL_ID"))
+
 let _client = new DiscordSocketClient()
 
 let mainTask =
     async {
         do!
-            _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"))
+            _client.LoginAsync(TokenType.Bot, token)
             |> awaitPlainTask
 
         do! _client.StartAsync() |> awaitPlainTask
@@ -26,6 +32,18 @@ let main _ =
     _client.add_Log
         (fun x ->
             async { do Console.WriteLine(x) }
+            |> startAsPlainTask)
+
+    // Handle incoming messages
+    _client.add_MessageReceived
+        (fun x ->
+            async {
+                do
+                    if (x.Channel.Id = channelId) then
+                        Console.WriteLine(x.Content)
+                    else
+                        ()
+            }
             |> startAsPlainTask)
 
     async {
